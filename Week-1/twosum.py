031dc0e8a61e3f94f149b414832076553b6ca3b7
Khulood <khuloodmuzaffar@gmail.com>
2024-01-06 22:43:00 +0500

    edited the brute force solution to fix the range of the outer loop

--- Week-1/twosum.py
+++ Week-1/twosum.py
@@ -43,7 +43,7 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
+        for i in range(len(nums) - 1):
             for j in range(i+1, len(nums)):
                 if (nums[i] + nums[j]) == target:
                     return [i, j]
